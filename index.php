a23340f1ee49b80888a91301fa5641160fe3a71f
veertos <125968688+veertos@users.noreply.github.com>
2024-04-20 11:04:31 +0530

    crud_php

--- index.php
+++ index.php
@@ -1,43 +1,69 @@
-<!-- PHP FOR CONNECTION & DATA INSERTION-->
-    <?php
+<!-- PHP FOR DATABASE CONNECTION & DATA INSERTION-->
+<?php
 
-        // SUCCESS POPUP VARIABLE INTIALIZE
-            $insert = false;
-        //SUCCESS POPUP VARIABLE INTIALIZE OVER
+    // SUCCESS POPUP VARIABLE INTIALIZATION
+    $insert = false;
+    $update = false;
+    $delete = false;
+    // SUCCESS POPUP VARIABLE INTIALIZATION OVER
 
-        //CONNECTION TO THE DATABASE
-            $servername = "localhost";
-            $username = "root";
-            $password = "";
-            $database = "notes";
+    // CONNECTION TO THE DATABASE
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "notes";
 
-            $conn = mysqli_connect($servername, $username, $password, $database);
+    $conn = mysqli_connect($servername, $username, $password, $database);
 
-            if(!$conn){
-                die("Connection Refused ".mysqli_connect_error());
+    // CONNECTION TO THE DATABASE OVER
+
+    // DATA DELETION
+    if (isset($_GET['delete'])) {
+        $sno = $_GET['delete'];
+        $delete = true;
+        $sql = "DELETE FROM `notes` WHERE `sno` = $sno";
+        $result = mysqli_query($conn, $sql);
+    }
+
+    // DATA INSERTION AND UPDATION
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        // DATA UPDATION
+        if (isset($_POST['snoEdit'])) {
+            $sno = $_POST['snoEdit'];
+            $title = $_POST['titleEdit'];
+            $description = $_POST['descriptionEdit'];
+
+            // SQL UPDATE statement
+            $sql = "UPDATE `notes` SET `title` = '$title', `description` = '$description' WHERE `sno` = $sno";
+
+            $result = mysqli_query($conn, $sql);
+
+            if ($result) {
+                $update = true;
+            } else {
+                echo "Not update" . mysqli_error($conn);
             }
-        //CONNECTION TO THE DATABASE OVER
+        }
 
-        //DATA INSERTION
-            if($_SERVER['REQUEST_METHOD']=='POST'){
-                    $title = $_POST['title'];
-                    $description = $_POST['description']; 
+        // DATA INSERTION
+        $title = $_POST['title'];
+        $description = $_POST['description'];
 
-                    $sql = "INSERT INTO `notes` (`sno`, `title`, `description`, `timestmp`) VALUES (NULL, '$title', '$description', current_timestamp());";
+        $sql = "INSERT INTO `notes` (`sno`, `title`, `description`, `timestmp`) VALUES (NULL, '$title', '$description', current_timestamp());";
 
-                $result = mysqli_query($conn, $sql);
+        $result = mysqli_query($conn, $sql);
 
-                if($result){
-                    $insert = true;
-                }
-                else{
-                    echo "data not created because of thisssssssssssssssssss".mysqli_error($conn);
-                }
-            }
-        //DATA INSERTION OVER
+        if ($result) {
+            $insert = true;
+        } else {
+            echo "data not created because of this" . mysqli_error($conn);
+        }
+    }
+    // DATA INSERTION AND UPDATION OVER
 
-    ?>
-<!-- PHP FOR CONNECTION & DATA INSERTION OVER--> 
+?>
+<!-- PHP FOR CONNECTION & DATA INSERTION OVER-->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -48,72 +74,77 @@
     <title>Todo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="//cdn.datatables.net/2.0.4/css/dataTables.dataTables.min.css">
-
     <style>
-    *{
-        outline: none;
-        outline-offset: 0px;
-    }
-  </style>
-
+        * {
+            outline: none;
+            outline-offset: 0px;
+        }
+    </style>
 </head>
 
 <body>
 
-    <!-- NAVBAR  -->
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                    </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
+    <!-- EDIT MODAL -->
+        <div class="modal fade" id="editModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="editModalLabel">Edit</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="" method="post">
+                        <div class="modal-body">
+                            <div class="container">
+                                <div class="row justify-content-center">
+                                    <div class="col-md-6 mt-5">
+                                        <h2 class="text-center mb-4">Add a Note</h2>
+                                        <input type="hidden" name="snoEdit" id="snoEdit">
+                                        <div class="form-group">
+                                            <label for="title">Note Title</label>
+                                            <input type="text" class="form-control" id="titleEdit" name="titleEdit" placeholder="Enter Note">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="description" class="form-label">Note description</label>
+                                            <textarea class="form-control" id="descriptionEdit" name="descriptionEdit" rows="3" placeholder="description"></textarea>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary my-2">Update Note</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Understood</button>
+                        </div>
                     </form>
                 </div>
             </div>
-        </nav>
-    <!-- NAVBAR OVER -->
-
+        </div>
+    <!-- EDIT MODAL OVER-->
 
     <!-- PHP FOR INSERTION SUCCESS UI  -->
-        <?php
-        
-            if($insert){
-                echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
-                <strong>Success!</strong> Your note inserted.
-                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
-            </div>";
-            }
-        
-        ?>
+    <?php
+        if ($insert) {
+            echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+                    <strong>Success!</strong> Your note inserted.
+                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                    </div>";
+        }
+        if ($update) {
+            echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+                    <strong>Success!</strong> Your note Updated.
+                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                    </div>";
+        }
+        if ($delete) {
+            echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+                    <strong>Success!</strong> Your note Deleted.
+                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                    </div>";
+        }
+    ?>
     <!-- PHP FOR INSERTION UI OVER-->
+
 
 
     <!-- FORM  -->
@@ -121,7 +152,7 @@
             <div class="row justify-content-center">
                 <div class="col-md-6 mt-5">
                     <h2 class="text-center mb-4">Add a Note</h2>
-                    <form action="/todo/index.php" method="post">
+                    <form action="/php_todo/index.php?update=true" method="post">
                         <div class="form-group">
                             <label for="title">Note Title</label>
                             <input type="text" class="form-control" id="title" name="title" placeholder="Enter Note">
@@ -142,41 +173,45 @@
         <div class="container">
 
             <table class="table" id="myTable">
-            <thead>
-                <tr>
-                <th scope="col">S.No</th>
-                <th scope="col">Title</th>
-                <th scope="col">description</th>
-                </tr>
-            </thead>
-            <tbody>
+                <thead>
+                    <tr>
+                        <th scope="col">S.No</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">description</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
 
 
-            <!-- PHP FOR SELECT AND TABLE UI  -->
-            <?php
+                    <!-- PHP FOR SELECT AND TABLE UI  -->
+                    <?php
 
                     $sql = "SELECT * FROM `notes`";
 
                     $result = mysqli_query($conn, $sql);
-                    
+
                     $sno = 0;
                     echo "<br>";
 
-                    while($row = mysqli_fetch_assoc($result)){
+                    while ($row = mysqli_fetch_assoc($result)) {
                         $sno = $sno++;
                         echo "<tr>
-                        <th scope='row'>".$row['sno']."</th>
-                        <td>".$row['title']."</td>
-                        <td>".$row['description']."</td>
-                    </tr>";
+                            <th scope='row'>" . $row['sno'] . "</th>
+                            <td>" . $row['title'] . "</td>
+                            <td>" . $row['description'] . "</td>
+                            <td><button class='edit btn btn-sm btn-primary' id =" . $row['sno'] . " >Edit</button> <button class='delete btn btn-sm btn-primary' id =d" . $row['sno'] . " >Delete</button></td>
+                        </tr>";
                     }
+
+
 
 
                     ?>
                     <!-- PHP FOR SELECT AND TABLE UI OVER -->
 
-                
-            </tbody>
+
+                </tbody>
             </table>
 
         </div>
@@ -184,7 +219,7 @@
 
 
 
-        
+
 
     <!-- BOOTSTRAP JS CDN -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -206,6 +241,41 @@
             let table = new DataTable('#myTable');
         </script>
     <!-- DATATABLE INTIALIZE SCRIPT OVER-->
+                
+
+    <!-- EDIT AND DELETE SCRIPT  -->
+        <script>
+            edits = document.getElementsByClassName('edit');
+            Array.from(edits).forEach((ele) => {
+                ele.addEventListener('click', (e) => {
+                    console.log("edit", );
+                    tr = e.target.parentNode.parentNode
+                    title = tr.getElementsByTagName('td')[0].innerHTML;
+                    description = tr.getElementsByTagName('td')[1].innerHTML;
+
+                    titleEdit.value = title;
+                    descriptionEdit.value = description;
+                    snoEdit.value = e.target.id
+                    $("#editModal").modal("toggle")
+                })
+            })
+            deletes = document.getElementsByClassName('delete');
+            Array.from(deletes).forEach((ele) => {
+                ele.addEventListener('click', (e) => {
+                    console.log("edit", );
+                    sno = e.target.id.substr(1, )
+
+                    if (confirm("Are you sure about delete!")) {
+                        console.log("yes");
+                        window.location = `/php_todo/index.php?delete=${sno}`
+                    } else {
+                        console.log("no");
+                    }
+                })
+            })
+        </script>
+    <!-- EDIT AND DELETE SCRIPT  -->
+
 
 
 </body>
